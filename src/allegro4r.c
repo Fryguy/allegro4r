f6d152ecadeb3325a51bffdfe4f06159feff2c1c
Fryguy <fryguy9@yahoo.com>
2009-02-07 02:04:35 -0500

    Added exmem.rb example (from exmem.c) Added API support for exmem.rb

--- src/allegro4r.c
+++ src/allegro4r.c
@@ -92,6 +92,22 @@ static VALUE al_SCREEN_H(VALUE self)
   return INT2FIX(SCREEN_H);
 }
 
+static VALUE al_create_bitmap(VALUE self, VALUE width, VALUE height)
+{
+  // TODO: Change to call destroy_bitmap on free?
+  BITMAP *bmp = create_bitmap(FIX2INT(width), FIX2INT(height));
+  VALUE obj = Data_Wrap_Struct(cBITMAP, 0, 0, bmp);
+  return obj;
+}
+
+static VALUE al_destroy_bitmap(VALUE self, VALUE bitmap)
+{
+  BITMAP *bmp;
+  Data_Get_Struct(bitmap, BITMAP, bmp);
+  destroy_bitmap(bmp);
+  return Qnil;
+}
+
 static VALUE al_acquire_screen(VALUE self)
 {
   acquire_screen();
@@ -130,13 +146,50 @@ static VALUE al_makecol(VALUE self, VALUE r, VALUE g, VALUE b)
   return INT2FIX(makecol(FIX2INT(r), FIX2INT(g), FIX2INT(b)));
 }
 
+static VALUE al_palette_color(VALUE self)
+{
+  // TODO: Cache the array, and only update if changed, or use hooked variable?
+  VALUE ary = rb_ary_new2(PAL_SIZE);
+  int x;
+  for (x = 0; x < PAL_SIZE; x++)
+    rb_ary_store(ary, x, INT2FIX(pallete_color[x]));
+  return ary;
+}
+
 // Drawing primitives
+
+static VALUE al_clear_bitmap(VALUE self, VALUE bitmap)
+{
+  BITMAP *bmp;
+  Data_Get_Struct(bitmap, BITMAP, bmp);
+  clear_bitmap(bmp);
+  return Qnil;
+}
 
 static VALUE al_clear_to_color(VALUE self, VALUE bitmap, VALUE color)
 {
   BITMAP *bmp;
   Data_Get_Struct(bitmap, BITMAP, bmp);
   clear_to_color(bmp, FIX2INT(color));
+  return Qnil;
+}
+
+static VALUE al_circle(VALUE self, VALUE bitmap, VALUE x, VALUE y, VALUE radius, VALUE color)
+{
+  BITMAP *bmp;
+  Data_Get_Struct(bitmap, BITMAP, bmp);
+  circle(bmp, FIX2INT(x), FIX2INT(y), FIX2INT(radius), FIX2INT(color));
+  return Qnil;
+}
+
+// Blitting and sprites
+
+static VALUE al_blit(VALUE self, VALUE source, VALUE dest, VALUE source_x, VALUE source_y, VALUE dest_x, VALUE dest_y, VALUE width, VALUE height)
+{
+  BITMAP *bmp_source, *bmp_dest;
+  Data_Get_Struct(source, BITMAP, bmp_source);
+  Data_Get_Struct(dest, BITMAP, bmp_dest);
+  blit(bmp_source, bmp_dest, FIX2INT(source_x), FIX2INT(source_y), FIX2INT(dest_x), FIX2INT(dest_y), FIX2INT(width), FIX2INT(height));
   return Qnil;
 }
 
@@ -154,10 +207,8 @@ static VALUE al_textout_centre_ex(VALUE self, VALUE bmp, VALUE f, VALUE s, VALUE
 {
   BITMAP *b;
   Data_Get_Struct(bmp, BITMAP, b);
-
   FONT *fnt;
   Data_Get_Struct(f, FONT, fnt);
-
   textout_centre_ex(b, fnt, StringValuePtr(s), FIX2INT(x), FIX2INT(y), FIX2INT(color), FIX2INT(bg));
   return Qnil;
 }
@@ -189,6 +240,8 @@ void Init_allegro4r()
   rb_define_module_function(modAllegro4r_API, "screen", al_screen, 0);
   rb_define_module_function(modAllegro4r_API, "SCREEN_W", al_SCREEN_W, 0);
   rb_define_module_function(modAllegro4r_API, "SCREEN_H", al_SCREEN_H, 0);
+  rb_define_module_function(modAllegro4r_API, "create_bitmap", al_create_bitmap, 2);
+  rb_define_module_function(modAllegro4r_API, "destroy_bitmap", al_destroy_bitmap, 1);
   rb_define_module_function(modAllegro4r_API, "acquire_screen", al_acquire_screen, 0);
   rb_define_module_function(modAllegro4r_API, "release_screen", al_release_screen, 0);
 
@@ -196,8 +249,13 @@ void Init_allegro4r()
   rb_define_module_function(modAllegro4r_API, "desktop_palette", al_desktop_palette, 0);
 
   rb_define_module_function(modAllegro4r_API, "makecol", al_makecol, 3);
+  rb_define_module_function(modAllegro4r_API, "palette_color", al_palette_color, 0);
 
+  rb_define_module_function(modAllegro4r_API, "clear_bitmap", al_clear_bitmap, 1);
   rb_define_module_function(modAllegro4r_API, "clear_to_color", al_clear_to_color, 2);
+  rb_define_module_function(modAllegro4r_API, "circle", al_circle, 5);
+
+  rb_define_module_function(modAllegro4r_API, "blit", al_blit, 8);
 
   rb_define_module_function(modAllegro4r_API, "font", al_font, 0);
   rb_define_module_function(modAllegro4r_API, "textout_centre_ex", al_textout_centre_ex, 7);
