f860375bcf26cb6f947e95049b525da5cd8d8819
Jason Frey <fryguy9@gmail.com>
2015-11-29 10:31:50 -0500

    Switch to global variable to more accurately represent the examples.

--- examples/ex_draw_bitmap.rb
+++ examples/ex_draw_bitmap.rb
@@ -45,15 +45,12 @@ class Example
     @frame_times = [0.0] * FPS
   end
 end
-
-def example
-  @example ||= Example.new
-end
+$example = Example.new
 
 def add_time
-  example.frame_times[example.ftpos] = al_get_time
-  example.ftpos += 1
-  example.ftpos = 0 if example.ftpos >= FPS
+  $example.frame_times[$example.ftpos] = al_get_time
+  $example.ftpos += 1
+  $example.ftpos = 0 if $example.ftpos >= FPS
 end
 
 def get_fps
@@ -62,8 +59,8 @@ def get_fps
   max_dt = 1 / 1000000.0
   av     = 0.0
   FPS.times do |i|
-    if i != example.ftpos
-      dt = example.frame_times[i] - example.frame_times[prev]
+    if i != $example.ftpos
+      dt = $example.frame_times[i] - $example.frame_times[prev]
       min_dt = dt if dt < min_dt
       max_dt = dt if dt > max_dt
       av += dt
@@ -79,15 +76,15 @@ def get_fps
 end
 
 def add_sprite
-  if example.sprite_count < MAX_SPRITES
-    w = al_get_display_width(example.display)
-    h = al_get_display_height(example.display)
-    i = example.sprite_count
-    example.sprite_count += 1
-    s = example.sprites[i]
+  if $example.sprite_count < MAX_SPRITES
+    w = al_get_display_width($example.display)
+    h = al_get_display_height($example.display)
+    i = $example.sprite_count
+    $example.sprite_count += 1
+    s = $example.sprites[i]
     a = rand(360).to_f
-    s.x = rand(w - example.bitmap_size).to_f
-    s.y = rand(h - example.bitmap_size).to_f
+    s.x = rand(w - $example.bitmap_size).to_f
+    s.y = rand(h - $example.bitmap_size).to_f
     s.dx = Math.cos(a) * FPS * 2
     s.dy = Math.sin(a) * FPS * 2
   end
@@ -98,32 +95,32 @@ def add_sprites(n)
 end
 
 def remove_sprites(n)
-  example.sprite_count -= n
-  example.sprite_count = 0 if example.sprite_count < 0
+  $example.sprite_count -= n
+  $example.sprite_count = 0 if $example.sprite_count < 0
 end
 
 def change_size(size)
   size = 1 if size < 1
   size = 1024 if size > 1024
 
-  al_destroy_bitmap(example.bitmap) if example.bitmap
+  al_destroy_bitmap($example.bitmap) if $example.bitmap
   al_set_new_bitmap_flags(
-    example.use_memory_bitmaps ? ALLEGRO_MEMORY_BITMAP : ALLEGRO_VIDEO_BITMAP)
-  example.bitmap = al_create_bitmap(size, size)
-  example.bitmap_size = size
-  al_set_target_bitmap(example.bitmap)
+    $example.use_memory_bitmaps ? ALLEGRO_MEMORY_BITMAP : ALLEGRO_VIDEO_BITMAP)
+  $example.bitmap = al_create_bitmap(size, size)
+  $example.bitmap_size = size
+  al_set_target_bitmap($example.bitmap)
   al_set_blender(ALLEGRO_ADD, ALLEGRO_ONE, ALLEGRO_ZERO)
   al_clear_to_color(al_map_rgba_f(0, 0, 0, 0))
-  bw = al_get_bitmap_width(example.mysha)
-  bh = al_get_bitmap_height(example.mysha)
-  al_draw_scaled_bitmap(example.mysha, 0, 0, bw, bh, 0, 0,
+  bw = al_get_bitmap_width($example.mysha)
+  bh = al_get_bitmap_height($example.mysha)
+  al_draw_scaled_bitmap($example.mysha, 0, 0, bw, bh, 0, 0,
     size, size, 0)
-  al_set_target_backbuffer(example.display)
+  al_set_target_backbuffer($example.display)
 end
 
 def sprite_update(s)
-  w = al_get_display_width(example.display)
-  h = al_get_display_height(example.display)
+  w = al_get_display_width($example.display)
+  h = al_get_display_height($example.display)
 
   s.x += s.dx / FPS
   s.y += s.dy / FPS
@@ -132,77 +129,77 @@ def sprite_update(s)
     s.x = -s.x
     s.dx = -s.dx
   end
-  if s.x + example.bitmap_size > w
-    s.x = -s.x + 2 * (w - example.bitmap_size)
+  if s.x + $example.bitmap_size > w
+    s.x = -s.x + 2 * (w - $example.bitmap_size)
     s.dx = -s.dx
   end
   if s.y < 0
     s.y = -s.y
     s.dy = -s.dy
   end
-  if s.y + example.bitmap_size > h
-    s.y = -s.y + 2 * (h - example.bitmap_size)
+  if s.y + $example.bitmap_size > h
+    s.y = -s.y + 2 * (h - $example.bitmap_size)
     s.dy = -s.dy
   end
 
-  s.x = w / 2 - example.bitmap_size / 2 if example.bitmap_size > w
-  s.y = h / 2 - example.bitmap_size / 2 if example.bitmap_size > h
+  s.x = w / 2 - $example.bitmap_size / 2 if $example.bitmap_size > w
+  s.y = h / 2 - $example.bitmap_size / 2 if $example.bitmap_size > h
 end
 
 def update
-  example.sprite_count.times do |i|
-    sprite_update(example.sprites[i])
+  $example.sprite_count.times do |i|
+    sprite_update($example.sprites[i])
   end
 end
 
 def redraw
-  w = al_get_display_width(example.display)
-  h = al_get_display_height(example.display)
-  fh = al_get_font_line_height(example.font)
+  w = al_get_display_width($example.display)
+  h = al_get_display_height($example.display)
+  fh = al_get_font_line_height($example.font)
   info = ["textures", "memory buffers"]
   binfo = ["alpha", "additive", "tinted", "solid"]
-  tint = example.white
+  tint = $example.white
 
-  case example.blending
+  case $example.blending
   when 0
     al_set_blender(ALLEGRO_ADD, ALLEGRO_ONE, ALLEGRO_INVERSE_ALPHA)
-    tint = example.half_white
+    tint = $example.half_white
   when 1
     al_set_blender(ALLEGRO_ADD, ALLEGRO_ONE, ALLEGRO_ONE)
-    tint = example.dark
+    tint = $example.dark
   when 2
     al_set_blender(ALLEGRO_ADD, ALLEGRO_ONE, ALLEGRO_ZERO)
-    tint = example.red
+    tint = $example.red
   when 3
     al_set_blender(ALLEGRO_ADD, ALLEGRO_ONE, ALLEGRO_ZERO)
   end
 
-  example.sprite_count.times do |i|
-    s = example.sprites[i]
-    al_draw_tinted_bitmap(example.bitmap, tint, s.x, s.y, 0)
+  $example.sprite_count.times do |i|
+    s = $example.sprites[i]
+    al_draw_tinted_bitmap($example.bitmap, tint, s.x, s.y, 0)
   end
 
   al_set_blender(ALLEGRO_ADD, ALLEGRO_ONE, ALLEGRO_INVERSE_ALPHA)
-  if example.show_help
+  if $example.show_help
     TEXT.each_with_index do |text, i|
-      al_draw_text(example.font, example.white, 0, h - 5 * fh + i * fh, 0, text)
+      al_draw_text($example.font, $example.white, 0, h - 5 * fh + i * fh, 0, text)
     end
   end
 
-  al_draw_textf(example.font, example.white, 0, 0, 0,
-    "count: #{example.sprite_count}")
-  al_draw_textf(example.font, example.white, 0, fh, 0,
-    "size: #{example.bitmap_size}")
-  al_draw_textf(example.font, example.white, 0, fh * 2, 0,
-    info[example.use_memory_bitmaps ? 1 : 0])
-  al_draw_textf(example.font, example.white, 0, fh * 3, 0,
-    binfo[example.blending])
+  al_draw_textf($example.font, $example.white, 0, 0, 0,
+    "count: #{$example.sprite_count}")
+  al_draw_textf($example.font, $example.white, 0, fh, 0,
+    "size: #{$example.bitmap_size}")
+  al_draw_textf($example.font, $example.white, 0, fh * 2, 0,
+    info[$example.use_memory_bitmaps ? 1 : 0])
+  al_draw_textf($example.font, $example.white, 0, fh * 3, 0,
+    binfo[$example.blending])
 
   f1, f2 = get_fps
-  al_draw_textf(example.font, example.white, w, 0, ALLEGRO_ALIGN_RIGHT, "FPS: %4d +- %-4d" %
+  al_draw_textf($example.font, $example.white, w, 0, ALLEGRO_ALIGN_RIGHT, "FPS: %4d +- %-4d" %
     [f1, f2])
-  dsm = 1.0 / example.direct_speed_measure
-  al_draw_textf(example.font, example.white, w, fh, ALLEGRO_ALIGN_RIGHT, "%4d / sec" %
+  dsm = 1.0 / $example.direct_speed_measure
+  al_draw_textf($example.font, $example.white, w, fh, ALLEGRO_ALIGN_RIGHT, "%4d / sec" %
     (dsm.finite? ? dsm.to_i : -1))
 end
 
@@ -211,7 +208,7 @@ def main
   w, h = 640, 480
   done = false
   need_redraw = true
-  example.show_help = true
+  $example.show_help = true
 
   abort_example("Failed to init Allegro.\n") unless al_init
 
@@ -224,23 +221,23 @@ def main
   al_get_monitor_info(0, info)
   w = info[:x2] - info[:x1] if info[:x2] - info[:x1] < w
   h = info[:y2] - info[:y1] if info[:y2] - info[:y1] < h
-  example.display = al_create_display(w, h)
-  abort_example("Error creating display.\n") if example.display.null?
+  $example.display = al_create_display(w, h)
+  abort_example("Error creating display.\n") if $example.display.null?
 
   abort_example("Error installing keyboard.\n") unless al_install_keyboard
 
   abort_example("Error installing mouse.\n") unless al_install_mouse
 
-  example.font = al_load_font("data/fixed_font.tga", 0, 0)
-  abort_example("Error loading data/fixed_font.tga\n") if example.font.null?
+  $example.font = al_load_font("data/fixed_font.tga", 0, 0)
+  abort_example("Error loading data/fixed_font.tga\n") if $example.font.null?
 
-  example.mysha = al_load_bitmap("data/mysha256x256.png")
-  abort_example("Error loading data/mysha256x256.png\n") if example.mysha.null?
+  $example.mysha = al_load_bitmap("data/mysha256x256.png")
+  abort_example("Error loading data/mysha256x256.png\n") if $example.mysha.null?
 
-  example.white = al_map_rgb_f(1, 1, 1)
-  example.half_white = al_map_rgba_f(1, 1, 1, 0.5)
-  example.dark = al_map_rgb(15, 15, 15)
-  example.red = al_map_rgb_f(1, 0.2, 0.1)
+  $example.white = al_map_rgb_f(1, 1, 1)
+  $example.half_white = al_map_rgba_f(1, 1, 1, 0.5)
+  $example.dark = al_map_rgb(15, 15, 15)
+  $example.red = al_map_rgb_f(1, 0.2, 0.1)
   change_size(256)
   add_sprite
   add_sprite
@@ -251,7 +248,7 @@ def main
   al_register_event_source(queue, al_get_keyboard_event_source)
   al_register_event_source(queue, al_get_mouse_event_source)
   al_register_event_source(queue, al_get_timer_event_source(timer))
-  al_register_event_source(queue, al_get_display_event_source(example.display))
+  al_register_event_source(queue, al_get_display_event_source($example.display))
 
   al_start_timer(timer)
 
@@ -263,7 +260,7 @@ def main
       al_clear_to_color(al_map_rgb_f(0, 0, 0))
       redraw
       t += al_get_time
-      example.direct_speed_measure = t
+      $example.direct_speed_measure = t
       al_flip_display
       need_redraw = false
     end
@@ -279,17 +276,17 @@ def main
       when ALLEGRO_KEY_DOWN
         remove_sprites(1)
       when ALLEGRO_KEY_LEFT
-        change_size(example.bitmap_size - 1)
+        change_size($example.bitmap_size - 1)
       when ALLEGRO_KEY_RIGHT
-        change_size(example.bitmap_size + 1)
+        change_size($example.bitmap_size + 1)
       when ALLEGRO_KEY_F1
-        example.show_help ^= true
+        $example.show_help ^= true
       when ALLEGRO_KEY_SPACE
-        example.use_memory_bitmaps ^= true
-        change_size(example.bitmap_size)
+        $example.use_memory_bitmaps ^= true
+        change_size($example.bitmap_size)
       when ALLEGRO_KEY_B
-        example.blending += 1
-        example.blending = 0 if example.blending == 4
+        $example.blending += 1
+        $example.blending = 0 if $example.blending == 4
       end
 
     when ALLEGRO_EVENT_DISPLAY_CLOSE
@@ -300,44 +297,44 @@ def main
       need_redraw = true
 
     when ALLEGRO_EVENT_MOUSE_BUTTON_DOWN
-      example.mouse_down = true
-      example.last_x = event[:mouse][:x]
-      example.last_y = event[:mouse][:y]
+      $example.mouse_down = true
+      $example.last_x = event[:mouse][:x]
+      $example.last_y = event[:mouse][:y]
 
     when ALLEGRO_EVENT_MOUSE_BUTTON_UP
-      fh = al_get_font_line_height(example.font)
-      example.mouse_down = false
+      fh = al_get_font_line_height($example.font)
+      $example.mouse_down = false
       if event[:mouse][:x] < 40 && event[:mouse][:y] >= h - fh * 5
         button = (event[:mouse][:y] - (h - fh * 5)) / fh
         case button
         when 0
-          example.use_memory_bitmaps ^= true
-          change_size(example.bitmap_size)
+          $example.use_memory_bitmaps ^= true
+          change_size($example.bitmap_size)
         when 1
-          example.blending += 1
-          example.blending = 0 if example.blending == 4
+          $example.blending += 1
+          $example.blending = 0 if $example.blending == 4
         when 4
-          example.show_help ^= true
+          $example.show_help ^= true
         end
       end
 
     when ALLEGRO_EVENT_MOUSE_AXES
-      if example.mouse_down
-        dx = event[:mouse][:x] - example.last_x
-        dy = event[:mouse][:y] - example.last_y
+      if $example.mouse_down
+        dx = event[:mouse][:x] - $example.last_x
+        dy = event[:mouse][:y] - $example.last_y
         add_sprites(dy / 4) if dy > 4
         remove_sprites(-dy / 4) if dy < -4
-        change_size(example.bitmap_size + dx - 4) if dx > 4
-        change_size(example.bitmap_size + dx + 4) if dx < -4
+        change_size($example.bitmap_size + dx - 4) if dx > 4
+        change_size($example.bitmap_size + dx + 4) if dx < -4
 
-        example.last_x = event[:mouse][:x]
-        example.last_y = event[:mouse][:y]
+        $example.last_x = event[:mouse][:x]
+        $example.last_y = event[:mouse][:y]
       end
 
     end
   end
 
-  al_destroy_bitmap(example.bitmap)
+  al_destroy_bitmap($example.bitmap)
 end
 
 run_main(method(:main))
